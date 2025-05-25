58601323fc31f653f4859b4d8b06bad625976679
Elliot Lichtman <doug.lichtman@gmail.com>
2025-05-24 21:18:56 -0700

    Update CodeLink 01.py
    Simplified the code by removing helper functions like makeGreen().  Also added the availableList variable, which will make the next step easier to program.

--- CodeLink 01.py
+++ CodeLink 01.py
@@ -3,29 +3,19 @@
 #
 # CodeLink 1
 # Wordle
-# last revised 12/25/24
+# last revised 5/24/25
 #
 # In this code, the computer guesses randomly.  Not great.
 # Can you implement our better algorithm by rewriting guessWord()?
-# You might need to add some other functions also.  For instance,
-# as you make guesses, you will want to eliminate incorrect words
-# from the wordList.
 #
 # Note that this demo uses only the 18 words from Chapter One. The idea
 # is to keep things simple so that we can really see what happens at
-# every step.  Once you have revised the code, however, feel free to
+# every step. Once you have revised the code, however, feel free to
 # replace the wordList with something much longer.
-
 
 import random
 
 # FUNCTIONS
-
-def makeGreen (letter):
-    return "G"
-
-def makeYellow (letter):
-    return "Y"
 
 def scoreGuess(guess, hiddenWord):
     # returns Y for yellow letters, G for green, blank otherwise
@@ -34,63 +24,96 @@ def scoreGuess(guess, hiddenWord):
     hiddenLettersUsed = [False, False, False, False, False]
     guessLettersUsed = [False, False, False, False, False]
     colors = ["_", "_", "_", "_", "_"]
-    
-    # go through the guess and makeGreen any matches
+
+    # go through the guess and make any matches green
     for letter in range(5):
         if guess[letter] == hiddenWord[letter]:
             hiddenLettersUsed[letter] = True
             guessLettersUsed[letter] = True
-            colors[letter] = makeGreen(guess[letter])
+            colors[letter] = "G"
 
-    # then go through the guess and makeYellow any remaining matches
+    # then go through the guess and make any remaining matches yellow
     for letter in range(5):
         if guessLettersUsed[letter] == False:
             for possibleMatch in range(5):
-                if guessLettersUsed[letter] == False and hiddenWord[possibleMatch] == guess[letter] and hiddenLettersUsed[possibleMatch] == False:
+                if guessLettersUsed[letter] == False and hiddenWord[possibleMatch] == guess[letter] and \
+                        hiddenLettersUsed[possibleMatch] == False:
                     guessLettersUsed[letter] = True
                     hiddenLettersUsed[possibleMatch] = True
-                    colors[letter] = makeYellow(guess[letter])
+                    colors[letter] = "Y"
 
-    return(colors)
+    return (colors)
+
+
+def hideWord(availableList):
+    # this function randomly chooses one word from the list
+
+    randomChoice = random.randint(0, len(availableList) - 1)
+    chosenWord = availableList[randomChoice]
+
+    return chosenWord
+
+
+def removeDuds(availableList, previousOutcome, previousGuess):
+    # remove words that could not have given us the previousOutcome
+    # also remove the word that was guessed unsuccessfully
+
+    newList = []
+
+    for word in availableList:
+        if scoreGuess(previousGuess, word) == previousOutcome:
+            if previousGuess != word:
+                newList.append(word)
+
+    return newList
+
+
+def guessWord(availableList, wordList):
+    # this function randomly chooses one word from the words that are still available
+    # you will replace this code with a better algorithm
+    # in that algorithm, the computer will guess the optimal word out of all the words the computer knows (wordList)
+    # and the computer will know that the hidden word is one of the words in availableList
     
-def hideWord(availableWords):
-    # this function randomly chooses one word from the list
-
-    randomChoice = random.randint(0,len(availableWords)-1)
-    chosenWord = availableWords[randomChoice]
+    randomChoice = random.randint(0, len(availableList) - 1)
+    chosenWord = availableList[randomChoice]
 
     return chosenWord
 
-def guessWord(availableWords):
 
-    # this function randomly chooses one word from the list
-    # you will replace this code with your better algorithm
-
-    randomChoice = random.randint(0,len(availableWords)-1)
-    chosenWord = availableWords[randomChoice]
-
-    return chosenWord
-   
 # ############
 # main program
 # ############
 
-wordList = ["adept","after","agent","avert","cater","eaten","eater","extra","hater","taken","taker","water","great","treat","wheat","taper","tread","tweak"]
+# make a list of all the words the computer knows
+wordList = ["adept", "after", "agent", "avert", "cater", "eaten", "eater", "extra", "hater", "taken", "taker", "water",
+            "great", "treat", "wheat", "taper", "tread", "tweak"]
 
-# initialize variables
-hiddenWord = hideWord(wordList)
+# make a separate list that will be updated to keep track of which words might still be the hidden word
+availableList = []
+for word in range(len(wordList)):
+    availableList.append(wordList[word])
+
+# initialize other variables
+hiddenWord = hideWord(availableList)
 guess = ""
 counter = 0
 
 print("")
 print("The hidden word is %s." % hiddenWord)
-print ("")
+print("")
 
 while guess != hiddenWord:
-
-    guess = guessWord(wordList)
+    guess = guessWord(availableList, wordList)
     counter = counter + 1
-    print ("Computer guesses: ", guess)
+    print("Computer guesses: ", guess)
+    outcome = scoreGuess(guess, hiddenWord)
+    availableList = removeDuds(availableList, outcome, guess)
+    print("  ", len(availableList), "words remaining.")
 
-print ("")
-print ("The computer needed %d guesses." % counter)
+    # failsafe to stop infinite loops
+    if counter > len(wordList):
+        print("Something has gone wrong - try again!")
+        break
+
+print("")
+print("The computer needed %d guesses." % counter)
